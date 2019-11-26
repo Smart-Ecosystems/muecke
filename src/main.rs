9fbaf3a18609f5c4003573c7d68c69ef91d8a2f2
zara <zgebru@thoughtworks.com>
2019-11-26 20:42:40 +0100

    Bala,Zara: start mqtt broker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate mosquitto_client as mosq;
+
+use mosq::Mosquitto;
+
+
 fn main() {
-    println!("Hello, world!");
+    let m = Mosquitto::new("test");
+
+    m.connect("broker.hivemq.com", 1883).expect("can't connect");
+    m.subscribe("bilbo/1", 1).expect("can't subscribe to bonzo");
+
+    let mut mc = m.callbacks(0);
+    mc.on_message(|data, msg| {
+        println!("Topic: {}, Msg: {:?}", msg.topic(), msg.text());
+        *data += 1;
+    });
+
+    m.loop_until_disconnect(200).expect("broken loop");
+    println!("received {} messages", mc.data);
 }
+
