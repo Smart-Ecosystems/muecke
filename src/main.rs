3e072f99896b28ab73a06cbaa0f8292d3597e89a
zara <zgebru@thoughtworks.com>
2019-11-26 20:54:06 +0100

    Bala,Zara: refactor mqtt client

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use mosq::Mosquitto;
 
 
 fn main() {
-    let m = Mosquitto::new("test");
+    run_mqtt();
+}
 
+
+fn run_mqtt() {
+    let m = Mosquitto::new("test");
     m.connect("broker.hivemq.com", 1883).expect("can't connect");
     m.subscribe("bilbo/1", 1).expect("can't subscribe to bonzo");
-
     let mut mc = m.callbacks(0);
     mc.on_message(|data, msg| {
         println!("Topic: {}, Msg: {:?}", msg.topic(), msg.text());
         *data += 1;
     });
-
     m.loop_until_disconnect(200).expect("broken loop");
     println!("received {} messages", mc.data);
 }
