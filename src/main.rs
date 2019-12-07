c242394392f39322b3ebc59c7e84e3cbcd47c9dc
Rahul De <rahul080327@gmail.com>
2019-12-07 02:36:41 +0100

    Rahul: Fix code, add cmd line arg for topic

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-extern crate mosquitto_client as mosq;
+extern crate mosquitto_client;
 
-use mosq::Mosquitto;
-
+use mosquitto_client::Mosquitto;
+use std::env;
+use std::process;
 
 fn main() {
-    run_mqtt();
-}
+    let m = Mosquitto::new("bilbo_baggins_1911");
+    let args = env::args().collect::<Vec<String>>();
 
+    if args.len() <= 1 {
+        println!("MQTT topic not provided.");
+        process::exit(1);
+    }
 
-fn run_mqtt() {
-    let m = Mosquitto::new("test");
-    m.connect("broker.hivemq.com", 1883).expect("can't connect");
-    m.subscribe("bilbo/1", 1).expect("can't subscribe to bonzo");
+    let topic = args[1].clone();
+
+    m.connect("broker.hivemq.com", 1883)
+        .expect("Can't connect to the broker.");
+    m.subscribe(&topic, 1)
+        .expect("can't subscribe to specified topic.");
+
     let mut mc = m.callbacks(0);
-    mc.on_message(|data, msg| {
-        println!("Topic: {}, Msg: {:?}", msg.topic(), msg.text());
-        *data += 1;
-    });
-    m.loop_until_disconnect(200).expect("broken loop");
-    println!("received {} messages", mc.data);
-}
 
+    mc.on_message(|_, msg| {
+        if !msg.retained() {
+            // Only read live, non-retained messages
+            println!("Received Msg {:?} on Topic {}.", msg.text(), msg.topic());
+        }
+    });
+
+    m.loop_until_disconnect(200).expect("Broken loop, exiting.");
+}
